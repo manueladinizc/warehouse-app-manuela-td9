9562417a43b4c54182605f160706c73d32d6f745
manueladinizc <manuela.mdc@gmail.com>
2022-09-16 14:45:33 -0300

    20 - Removendo repetições

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,7 +1,8 @@
 class WarehousesController < ApplicationController
+    
+    before_action :set_warehouse, only: [:show, :edit, :update]
+    
     def show
-        id = params[:id]
-        @warehouse = Warehouse.find(id)
     end
 
     def new
@@ -9,8 +10,6 @@ class WarehousesController < ApplicationController
     end
 
     def create
-        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area) #Strong parametres
-
         @warehouse = Warehouse.new(warehouse_params)
         if @warehouse.save()
             redirect_to root_path, notice: 'Galpão cadastrado com sucesso.'
@@ -21,20 +20,24 @@ class WarehousesController < ApplicationController
     end
 
     def edit
-        id = params[:id]
-        @warehouse = Warehouse.find(id)
     end
 
-    def update
-        id = params[:id]
-        @warehouse = Warehouse.find(id)
-        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
-        
+    def update       
         if @warehouse.update(warehouse_params)
         redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso'
         else
             flash.now[:notice] = 'Não foi possível atualizar o galpão'
             render 'edit'
         end
+    end
+        #Para evitar repetir nas actions a rotas que precisa passar o id, será criado um metodo.
+        #Para definir que o metodo criado num controller não é action e só pode ser usado dentro dessa classe, deve-se colocar o private.
+    private
+    def set_warehouse 
+        @warehouse = Warehouse.find(params[:id])
+    end    
+
+    def warehouse_params
+        params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area) #strong params
     end
 end
