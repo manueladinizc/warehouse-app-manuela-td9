ac9ca0643a2699fe9bc09894174b4e7a0b8e0a03
manueladinizc <manuela.mdc@gmail.com>
2022-09-19 11:15:01 -0300

    regex cep

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,5 +1,5 @@
 class Warehouse < ApplicationRecord
     validates :name, :code, :city, :description, :address, :cep, :area, presence: true
-    validates :code, uniqueness: true
-    
+    validates :name, :code, uniqueness: true
+    validates :cep, format: {with: /\d{5}-\d{3}/, message: "Formato deve ser XXXXX-XXX"}
 end
