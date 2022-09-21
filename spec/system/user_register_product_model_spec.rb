1b8afdf134848435e6b04179bdd34b8f61470d44
manueladinizc <manuela.mdc@gmail.com>
2022-09-21 16:25:32 -0300

    Bloqueando visitantes em rotas

--- spec/system/user_register_product_model_spec.rb
+++ spec/system/user_register_product_model_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 describe 'Usuário cadastra um modelo de produto' do
     it 'com sucesso' do
         #Arrange
+        user = User.create!(name: "Joao", email: 'joao@email.com', password: 'password')
+
         supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '4207427100013',full_address: 'Av Nacoes Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
         other_supplier = Supplier.create!(brand_name: 'LG', corporate_name: 'LG do Brasil LTDA', registration_number: '9207427100015',full_address: 'Av Ibirapuera, 300', city: 'São Paulo', state: 'SP', email: 'contato@lg.com.br')
         #Act
         visit root_path
-        click_on 'Modelos de Produtos'
+        login_as(user)
+        
+        within('nav') do 
+            click_on 'Modelos de Produtos'
+        end 
         click_on 'Cadastrar Novo'
         fill_in 'Nome', with: 'TV 40 polegadas'
         fill_in 'Peso', with: 10000
@@ -31,11 +37,17 @@ describe 'Usuário cadastra um modelo de produto' do
 
     it 'deve preencher todos os campos' do
          #Arrange
+         user = User.create!(name: "Joao", email: 'joao@email.com', password: 'password')
+
          supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '4207427100013',full_address: 'Av Nacoes Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
          
          #Act
          visit root_path
-         click_on 'Modelos de Produtos'
+         login_as(user)
+        
+         within('nav') do 
+            click_on 'Modelos de Produtos'
+         end 
          click_on 'Cadastrar Novo'
          fill_in 'Nome', with: ''
          fill_in 'SKU', with: ''
--- spec/system/user_view_product_models_spec.rb
+++ spec/system/user_view_product_models_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 describe 'Usuário vê models de produtos' do
+   it 'se estiver autenticado' do
+    #Arrange
+    
+    #Act
+    visit root_path
+    
+    within('nav') do
+        click_on 'Modelos de Produtos'
+    end
+    #Assert
+    expect(current_path).to eq new_user_session_path
+
+   end
+    
     it 'a partir do menu' do
         #Arrange
-
+        user = User.create!(name: "Joao", email: 'joao@email.com', password: 'password')
         #Act
         visit root_path
+        login_as(user)
         within('nav') do 
             click_on 'Modelos de Produtos'
         end 
@@ -15,12 +30,15 @@ describe 'Usuário vê models de produtos' do
 
     it 'com sucesso' do 
         #Arrange
+        user = User.create!(name: "Joao", email: 'joao@email.com', password: 'password')
+
         supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '4207427100013',full_address: 'Av Nacoes Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
 
         ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth:10, sku:'0245-TV32CCdddDDDeee', supplier: supplier)
         ProductModel.create!(name: 'SoundBar 7.1 Surround', weight: 3000, width: 80, height: 15, depth:20, sku:'0245-SOU2CCdddDDDeee', supplier: supplier)
-        #Act
+        #Act        
         visit root_path
+        login_as(user)
         within('nav') do 
             click_on 'Modelos de Produtos'
         end
@@ -34,10 +52,12 @@ describe 'Usuário vê models de produtos' do
 
     end
 
-    it ' e não existm produtos cadastrados' do
+    it ' e não existem produtos cadastrados' do
         #Arrange
+        user = User.create!(name: "Joao", email: 'joao@email.com', password: 'password')
         #Act
         visit root_path
+        login_as(user)
         click_on 'Modelos de Produtos'
         #Assert
         expect(page).to have_content 'Nenhum modelo de produto cadastrado.'
