02f010b28e5e19c11029c09e47322eab87f83187
manueladinizc <manuela.mdc@gmail.com>
2022-09-23 14:16:12 -0300

    Validações personalizadas

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,19 @@ class Order < ApplicationRecord
   belongs_to :supplier
   belongs_to :user
 
-  validates :code, presence: true
-
+  validates :code, :estimated_delivery_date, presence: true
+  validate :estimated_delivery_date_is_future
   before_validation :generate_code #criar o código antes da validação acontecer para garantir que o código está prsente
   
   private
   def generate_code
     self.code = SecureRandom.alphanumeric(8).upcase
   end
+
+  def estimated_delivery_date_is_future
+    if self.estimated_delivery_date.present? && self.estimated_delivery_date <= Date.today
+      self.errors.add(:estimated_delivery_date, "deve ser futura.")
+    end
+  end
+
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,21 +2,71 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
    describe '#valid?' do
-   it 'deve ter um código' do
-    #Arrange
-    u = User.create!(name:'Julia Almeida', email: 'julia@yahoo.com', password:'12345678')
+      it 'deve ter um código' do
+      #Arrange
+      u = User.create!(name:'Julia Almeida', email: 'julia@yahoo.com', password:'12345678')
 
-    w = Warehouse.create!(name: 'Santos Dumont', code: 'RIO', address: 'Endereço', cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição', state: "RJ" )
+      w = Warehouse.create!(name: 'Santos Dumont', code: 'RIO', address: 'Endereço', cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição', state: "RJ" )
 
-    s = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number:'7584563215984', full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+      s = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number:'7584563215984', full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
 
-    order = Order.new(user: u, warehouse:w, supplier: s, estimated_delivery_date: '2022-10-01')
+      order = Order.new(user: u, warehouse:w, supplier: s, estimated_delivery_date: '2022-10-01')
 
-    #Act
-    result = order.valid?
-    #Assert
-    expect(result).to be true
-   end
+      #Act
+      result = order.valid?
+      #Assert
+      expect(result).to be true
+      end
+
+      it 'data estimada de entrega deve ser obrigatória' do
+         #Arrange
+         # user = User.create!(name:'Julia Almeida', email: 'julia@yahoo.com', password:'12345678')
+
+         # warehouse = Warehouse.create!(name: 'Santos Dumont', code: 'RIO', address: 'Endereço', cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição', state: "RJ" )
+
+         # supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number:'7584563215984', full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+         
+         #order = Order.new(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: '')
+
+         #"como se deseja verificar apenas um campo especifico, e o teste especifica isso no Act, é criar o order apenas verificando o campo desejado" 
+
+         order = Order.new(estimated_delivery_date: '')
+         #Act
+         order.valid?
+         result = order.errors.include?(:estimated_delivery_date)
+         #Assert
+         expect(result).to be true
+      end
+
+      it 'data estimada de entrega não deve ser passada' do
+         #Arrange
+         order = Order.new(estimated_delivery_date: 1.day.ago)
+         #Act
+         order.valid?         
+         #Assert
+         expect(order.errors.include?(:estimated_delivery_date)).to be true
+         expect(order.errors[:estimated_delivery_date]).to include("deve ser futura.")
+      end
+
+      it 'data estimada de entrega não deve ser igual a hoje' do
+         #Arrange
+         order = Order.new(estimated_delivery_date: Date.today)
+         #Act
+         order.valid?
+         #Assert
+         expect(order.errors.include?(:estimated_delivery_date)).to be true
+         expect(order.errors[:estimated_delivery_date]).to include("deve ser futura.")
+      end
+
+      it 'data estimada de entrega deve ser igual ou maior que amanhã' do
+         #Arrange
+         order = Order.new(estimated_delivery_date: 1.day.from_now)
+         #Act
+         order.valid?
+         #Assert
+         expect(order.errors.include?(:estimated_delivery_date)).to be false
+      end
+
   end
   
   describe 'gera um código aleatório' do
