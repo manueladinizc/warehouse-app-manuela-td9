b5825f75b86f78c0fac31240c30fcf40d4072430
manueladinizc <manuela.mdc@gmail.com>
2022-09-23 14:21:48 -0300

    melhoria validação peso e dimensões

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -4,28 +4,28 @@ class ProductModel < ApplicationRecord
   validates :sku, format: {with: /\A[a-zA-Z0-9-]{20}\z/, message: "Deve conter 20 caracteres"}
   validate :min_weight, :min_width, :min_height, :min_depth
   validates :sku, uniqueness: true
-
+ 
   def min_weight
-    if (weight.nil? || weight <= 0)
-      errors.add(:weight, "deve ser maior que 0")
+    if (self.weight.present? && self.weight <= 0)
+      self.errors.add(:weight, "deve ser maior que 0")
        end
   end
 
   def min_width
-    if (width.nil? || width <= 0)
-       errors.add(:width, "deve ser maior que 0")
+    if (self.width.present? && self.width <= 0)
+       self.errors.add(:width, "deve ser maior que 0")
        end
   end
 
   def min_height
-    if (height.nil? || height <= 0)
-      errors.add(:height, "deve ser maior que 0")
+    if (self.height.present? && self.height <= 0)
+      self.errors.add(:height, "deve ser maior que 0")
     end
   end
 
   def min_depth
-    if (depth.nil? || depth <= 0)
-      errors.add(:depth, "deve ser maior que 0")
+    if (self.depth.present? && self.depth <= 0)
+      self.errors.add(:depth, "deve ser maior que 0")
        end
   end
 
