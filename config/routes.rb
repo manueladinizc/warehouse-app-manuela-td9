90e06eab8a09cd5f72e7978be20b55e2a4479cc8
manueladinizc <manuela.mdc@gmail.com>
2022-09-29 00:57:48 -0300

    43 - Saída de itens do estoque

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   # get '/', to: 'home#index'
-  resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :stock_product_destinations, only: [:create] #Como o formulário estána mesma tela show earehouse, só precisa do create, não precisa do new
+  end
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
   resources :product_models, only: [:index, :new, :create, :show]
   
@@ -14,4 +16,6 @@ Rails.application.routes.draw do
     post 'delivered', on: :member
     post 'canceled', on: :member
   end
+
+  
 end
--- spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
+++ spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
@@ -24,9 +24,37 @@ describe 'Usuário vê o estoque' do
         visit root_path
         click_on 'Galpão SP'
         #Assert
-        expect(page).to have_content 'Itens em Estoque'
-        expect(page).to have_content '3 x 1245-TV32CCdddDDDeee'
-        expect(page).to have_content '2 x 3245-NTI5CCdddDDDeee'
-        expect(page).not_to have_content '0245-SB71CCdddDDDeee'
+        within("section#stock_products") do
+            expect(page).to have_content 'Itens em Estoque'
+            expect(page).to have_content '3 x 1245-TV32CCdddDDDeee'
+            expect(page).to have_content '2 x 3245-NTI5CCdddDDDeee'
+            expect(page).not_to have_content '0245-SB71CCdddDDDeee'
+        end
+    end
+
+    it 'e dá baixa em um item' do
+        #Arrange
+        user = User.create!(name: 'Andre', email: 'andre@email.com', password: 'password')
+        
+        warehouse = Warehouse.create!(name: 'Galpão SP', code:'GRU', city:'São Paulo', area: '60_000', address: 'Av Paulista, 1000', cep: '80000-000', description: 'Galpão SP', state: "SP")
+
+        supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA', registration_number: '4207427100013',full_address: 'Av Nacoes Unidas, 1000', city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    
+        order = Order.create!(user: user, supplier: supplier, warehouse: warehouse, estimated_delivery_date: 1.day.from_now)
+
+        product_tv = ProductModel.create!(name: 'TV 40 Polegadas', weight: 8000, width: 70, height: 45, depth:10, sku:'1245-TV32CCdddDDDeee', supplier: supplier)
+
+        2.times { StockProduct.create!(order: order, warehouse: warehouse, product_model: product_tv) }
+        #Act
+        login_as user
+        visit root_path
+        click_on 'Galpão SP'
+        select '1245-TV32CCdddDDDeee', from: 'Item para Saída'
+        fill_in 'Destinatário', with: 'Maria Ferreira'
+        fill_in 'Endereço Destino', with: 'Rua das Palmeiras, 100 - Campinas - São Paulo'
+        click_on 'Confirmar Retirada'
+        #Assert
+        expect(current_path).to eq warehouse_path(warehouse.id)
+        expect(page).to have_content 'Item retirado com sucesso'
     end
 end
