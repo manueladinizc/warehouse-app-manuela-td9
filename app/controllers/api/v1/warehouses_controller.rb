966e6ededab563289556793f54e1560abbea9275
manueladinizc <manuela.mdc@gmail.com>
2022-10-25 18:19:09 -0300

    APIs: listando todos galpões

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,8 +1,27 @@
 class Api::V1::WarehousesController < ActionController::API
     def show
-        warehouse = Warehouse.find(params[:id])
-        render status: 200, json: warehouse
-        #render status: 200, json: "{ }"
-        #metodo, status http desejado, conteúdo json
+        #quando o código puder apresentar algum erro se usa "begin/rescue"
+        #warehouse = Warehouse.find(params[:id])
+        
+        
+        #warehouse = Warehouse.find_by(id: params[:id]) #devolve nil
+        #if warehouse.nil?
+            #return render status: 404
+        #end   
+            #return render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
+            #render status: 200, json: "{ }"
+            #metodo, status http desejado, conteúdo json
+        
+            begin #a seguir as linhas que podem dar erro"
+                warehouse = Warehouse.find(params[:id]) #para o find, precisa fazer o tratamento de erros
+                return render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
+            rescue #a seguir em caso de erro, aslinhas que serão mostradas
+                render status: 404
+            end   
+    end
+    def index
+        warehouses = Warehouse.all.order(:name)
+        #render status: 200, json: "[]" #objeto vazio
+        render status: 200, json: warehouses #converte os warehouses em json
     end
 end
--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -10,9 +10,49 @@ describe 'Warehouse API' do
             #Assert
             expect(response.status).to eq 200
             expect(response.content_type).to include "application/json"
-            json_response = JSON.parse(response.body) #formatar o corpo da repsosta como JSON. O conteúdo da resposta é convertido para JSON
+            json_response = JSON.parse(response.body) #formatar o corpo da resposta como JSON. O conteúdo da resposta é convertido para JSON
             expect(json_response["name"]).to eq('Aeroporto SP')
             expect(json_response["code"]).to eq('GRU')
+            expect(json_response.keys).not_to include("created_at")
+            expect(json_response.keys).not_to include("updated_at")
+        end
+        
+        it 'fail warehouse not found' do
+            #Arrange
+            #Assert
+            get "/api/v1/warehouses/99999"
+            #Act
+            expect(response.status).to eq 404
+        end
+    end
+
+    context 'GET /api/v1/warehouses' do
+        it 'list all warehouses ordered by name' do
+            #Arrange
+            Warehouse.create!(name: 'Galpão SP', code:'GRU', city:'São Paulo', area: '60_000', address: 'Av Paulista, 1000', cep: '80000-000', description: 'Galpão SP', state: "SP")
+
+            Warehouse.create!(name: 'Galpão Rio', code:'RIO', city:'Rio de Janeiro', area: '60_000', address: 'Av mar, 1000', cep: '90000-000', description: 'Galpão RIO', state: "RJ")
+            #Act
+            get '/api/v1/warehouses'
+            #Assert
+            expect(response.status).to eq 200
+            expect(response.content_type).to include 'application/json'
+            json_response = JSON.parse(response.body) #conteudo body vazio foi passado no controllercomo objeto, converte numa variável usando o modo JSON do ruby            
+            expect(json_response.length).to eq 2
+            expect(json_response[1]["name"]).to eq "Galpão SP"
+            expect(json_response[0]["name"]).to eq "Galpão Rio"
+
+        end
+
+        it 'return empty if is no warehouse' do
+            #Act
+            get '/api/v1/warehouses'
+            expect(response.status).to eq 200
+            expect(response.content_type).to include 'application/json'
+            json_response = JSON.parse(response.body)
+            expect(json_response).to eq []
+
+
         end
     end
 end
